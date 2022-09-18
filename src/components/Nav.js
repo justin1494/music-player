4873eeee58b3cfa957e0b4e42f787ae25dad7008
Filip Paczkowski <filip.paczkowski@esatto.se>
2022-09-18 18:13:07 +0200

    Data flow changes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,11 +2,11 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMusic } from "@fortawesome/free-solid-svg-icons";
 
-export const Nav = ({ libraryStatus, setLibraryStatus }) => {
+export const Nav = ({ toggleLibraryStatus }) => {
   return (
     <nav>
       <h1>Waves</h1>
-      <button onClick={() => setLibraryStatus(!libraryStatus)}>
+      <button onClick={() => toggleLibraryStatus()}>
         Library
         <FontAwesomeIcon icon={faMusic} />
       </button>
